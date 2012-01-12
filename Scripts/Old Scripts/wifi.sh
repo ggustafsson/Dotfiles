e15129dbb3deba9e4ba0a66161f9c160293ba753
Göran Gustafsson <gustafsson.g@gmail.com>
2012-01-12 11:08:02 +0100

    ...
    ...

--- Scripts/Old Scripts/wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Written by Göran Gustafsson <gustafsson.g@gmail.com>.
-
-ip link set wlan0 up
-wpa_supplicant -D nl80211 -i wlan0 -c /etc/wpa_supplicant.conf &
-sleep 5 && dhcpcd wlan0
-
