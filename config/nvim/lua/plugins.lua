0e8f5f7e51443d67ef836a09623536745971fb79
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-08 22:16:11 +0200

    nvim-cmp config

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,4 +1,22 @@
-require("comment").setup {
+require("cmp").setup({
+  sources = {
+    { name = "buffer" },
+    { name = "nvim_lsp" },
+    { name = "path" },
+  },
+})
+require("cmp").setup.cmdline(":", {
+  sources = {
+    { name = "cmdline" },
+  },
+})
+require("cmp").setup.cmdline("/", {
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+require("comment").setup({
   toggler = {
     line = "gcc",
     block = "gCC",
@@ -7,6 +25,6 @@ require("comment").setup {
     line = "gc",
     block = "gC",
   },
-}
+})
 
 require("gitsigns").setup()
