1eb85ccc1ab76a85869dc89ac34f02a3919de591
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-09 07:26:18 +0200

    textobjects

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -26,4 +26,19 @@ require("nvim-treesitter.configs").setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+
+  textobjects = {
+    select = {
+      enable = true,
+
+      lookahead = true,
+
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+  },
 }
