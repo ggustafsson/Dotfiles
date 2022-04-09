5857686736090f440489533dda951d23add618a6
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-09 14:28:35 +0200

    trouble

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -43,3 +43,18 @@ require("nvim-treesitter.configs").setup({
     },
   },
 })
+
+require("trouble").setup({
+  icons = false,
+  fold_open = "v",
+  fold_closed = ">",
+  indent_lines = false,
+
+  signs = {
+    error = "error",
+    warning = "warn",
+    hint = "hint",
+    information = "info",
+  },
+  use_diagnostic_signs = false,
+})
