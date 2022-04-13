0e43390715f236cc956002c0917ebea3b2cc65cf
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-13 17:31:33 +0200

    -- ansiblels

--- config/nvim/lua/language-servers.lua
+++ config/nvim/lua/language-servers.lua
@@ -4,8 +4,9 @@ local lspconfig = require "lspconfig"
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
+-- FIXME: Uncomment "ansiblels" after https://github.com/Homebrew/homebrew-core/pull/99170
 local servers = {
-  "ansiblels",
+  -- "ansiblels",
   "bashls",
   "gopls",
   "pyright",
@@ -17,7 +18,7 @@ for _, lsp in ipairs(servers) do
   }
 end
 
-lspconfig.ansiblels.setup {}
+-- lspconfig.ansiblels.setup {}
 lspconfig.bashls.setup {}
 lspconfig.gopls.setup {}
 lspconfig.pyright.setup {}
