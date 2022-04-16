0bd99ac741f9e25f3cfcb940c91454f3445b68b2
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-16 17:39:50 +0200

    vim.api.nvim_create_autocmd()

--- config/nvim/lua/linters-fmt.lua
+++ config/nvim/lua/linters-fmt.lua
@@ -21,12 +21,11 @@ null_ls.setup {
 
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
-      vim.cmd [[
-        augroup LspFormatting
-          autocmd! * <buffer>
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-        augroup END
-      ]]
+      local group = vim.api.nvim_create_augroup("LspFormatting", {})
+      vim.api.nvim_create_autocmd("BufWritePre <buffer>", {
+        command = "lua vim.lsp.buf.formatting_sync()",
+        group = group,
+      })
     end
   end,
 }
