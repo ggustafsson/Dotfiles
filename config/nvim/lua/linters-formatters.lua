2e6d86e13ed4937a6be58021be53b5d7aeecd77e
Göran Gustafsson <gustafsson.g@gmail.com>
2022-08-14 08:00:54 +0200

    Commit and run...

--- config/nvim/lua/linters-formatters.lua
+++ config/nvim/lua/linters-formatters.lua
@@ -3,7 +3,6 @@ local null_ls = require "null-ls"
 local sources = {
   null_ls.builtins.diagnostics.flake8,
   null_ls.builtins.diagnostics.pylint,
-  null_ls.builtins.diagnostics.staticcheck,
   null_ls.builtins.diagnostics.tidy,
   null_ls.builtins.diagnostics.yamllint,
   null_ls.builtins.diagnostics.zsh,
