0ccb9e9d4d12b5ba4ef7c1bcb3941dfa61bc8d2a
Göran Gustafsson <gustafsson.g@gmail.com>
2022-10-16 13:40:44 +0200

    -capabilities

--- config/nvim/lua/languages.lua
+++ config/nvim/lua/languages.lua
@@ -1,3 +1,5 @@
+local lspconfig = require "lspconfig"
+
 local servers = {
   "ansiblels", -- Includes "ansible-lint".
   "bashls",    -- Includes "ShellCheck".
@@ -25,15 +27,8 @@ local on_attach = function(client, bufnr)
   })
 end
 
-local lspconfig = require "lspconfig"
-local cmp_nvim_lsp = require "cmp_nvim_lsp"
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
-
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    capabilities = capabilities,
     on_attach = on_attach,
   }
 end
